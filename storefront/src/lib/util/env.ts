52d63d13ee358912af6e75ea84a538ee95c15d19
Roshikkkkk <roshikdev@gmail.com>
2025-06-22 01:54:38 -0700

    Update storefront and backend

--- storefront/src/lib/util/env.ts
+++ storefront/src/lib/util/env.ts
@@ -1,3 +1,6 @@
-export const getBaseURL = () => {
-  return process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
+export function getBaseURL() {
+  if (typeof window !== "undefined") {
+    return `http://${window.location.hostname}:8000`;
+  }
+  return process.env.NEXT_PUBLIC_BASE_URL?.split(",")[0] || "http://localhost:8000";
 }
