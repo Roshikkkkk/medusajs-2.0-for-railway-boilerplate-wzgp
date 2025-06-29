0dfc39a8cd3ec2402ea53868a95f91111b23c6a7
Roshikkkkk <roshikdev@gmail.com>
2025-06-28 20:55:27 -0700

    Restore original state

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -1,5 +1,5 @@
 import { loadEnv, Modules, defineConfig } from '@medusajs/utils';
-import { ADMIN_CORS, AUTH_CORS, BACKEND_URL, COOKIE_SECRET, DATABASE_URL, JWT_SECRET, REDIS_URL, RESEND_API_KEY, RESEND_FROM_EMAIL, SENDGRID_API_KEY, SENDGRID_FROM_EMAIL, SHOULD_DISABLE_ADMIN, STORE_CORS, STRIPE_API_KEY, STRIPE_WEBHOOK_SECRET, WORKER_MODE, MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, MINIO_BUCKET, MEILISEARCH_HOST, MEILISEARCH_ADMIN_KEY } from 'lib/constants';
+import { ADMIN_CORS, AUTH_CORS, BACKEND_URL, COOKIE_SECRET, DATABASE_URL, JWT_SECRET, REDIS_URL, RESEND_API_KEY, RESEND_FROM_EMAIL, SENDGRID_API_KEY, SENDGRID_FROM_EMAIL, SHOULD_DISABLE_ADMIN, STORE_CORS, STRIPE_API_KEY, STRIPE_WEBHOOK_SECRET, WORKER_MODE, MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, MINIO_BUCKET } from 'lib/constants';
 
 loadEnv(process.env.NODE_ENV, process.cwd());
 
@@ -36,24 +36,14 @@ const medusaConfig = {
             resolve: '@medusajs/file-s3',
             id: 'minio',
             options: {
-              s3_url: MINIO_ENDPOINT,
-              bucket: MINIO_BUCKET || 'medusa-media', // Указываем твой бакет
+              s3_url: `https://${MINIO_ENDPOINT}`, // Исправляем протокол
+              bucket: MINIO_BUCKET || 'medusa-media',
               aws_access_key_id: MINIO_ACCESS_KEY,
               aws_secret_access_key: MINIO_SECRET_KEY,
               region: 'us-east-1',
             },
           },
         ],
-      },
-    },
-    {
-      key: Modules.SEARCH,
-      resolve: '@medusajs/search-meilisearch',
-      options: {
-        config: {
-          host: MEILISEARCH_HOST,
-          apiKey: MEILISEARCH_ADMIN_KEY,
-        },
       },
     },
   ],
